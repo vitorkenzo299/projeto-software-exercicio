0b81ef1d0d63ac70363d28cf32eded1cfb83381d
Vitor Fengler <vitor.fengler7@gmail.com>
2025-09-15 10:57:15 -0300

    correcao

---
+++ src/main/java/br/edu/insper/pessoa/PessoaApplication.java
@@ -0,0 +1,13 @@
+package br.edu.insper.pessoa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PessoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PessoaApplication.class, args);
+	}
+
+}
